6680eef5099ee36a0b0336f3b637e7d0735e98eb
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-07-08 09:36:38 +0100

    router modifications

--- src/router.js
+++ src/router.js
@@ -1,25 +1,49 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET
+console.log(secret)
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
 
+  if (username === mockUser.username && password === mockUser.password) {
+    const token = jwt.sign(mockUser.username, secret);
+
+    res.json({
+      token,
+    });
+  } else {
+    res.json({
+      error: "Incorrect Username, Password combination",
+    });
+  }
 });
 
-router.get('/profile', (req, res) => {
-  
-});
+router.get("/profile", (req, res) => {
+  const token = req.headers.authorization;
 
+  try {
+    jwt.verify(token, secret);
+    res.json({
+      profile: mockUser.profile,
+    });
+  } catch (err) {
+    res.status(401).json({
+      error: "Not Authorised",
+    });
+  }
+});
 
 module.exports = router;
